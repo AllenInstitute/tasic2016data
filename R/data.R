5d15e3fbc5256262c53f764e9a19adc5b0f5d12a
Lucas Gray <soulcatalyst@gmail.com>
2018-01-15 16:16:05 -0800

    Typo fix for tasic_2016_anno

--- R/data.R
+++ R/data.R
@@ -17,7 +17,7 @@
 #'  \item{core_intermediate}{Flag for cells that are assigned to "core" or "intermediate" cell type identity based on random forest analysis}
 #'  \item{primary_type}{The primary cell type assigned to each cell}
 #'  \item{secondary_type}{If a cell is intermediate, the secondary cell type assigned to each cell. If the cell is core, this will be blank ("")}
-#'  \itme{aibs_vignette_id}{A unique short cell identifier that is used in the Allen Brain Institute Vignette at http://casestudies.brain-map.org/celltax }
+#'  \item{aibs_vignette_id}{A unique short cell identifier that is used in the Allen Brain Institute Vignette at http://casestudies.brain-map.org/celltax }
 #' }
 "tasic_2016_anno"
 
